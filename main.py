50a5fb336f866cc0349efd71476409673a382709
Leandro Carriego <leandrocarriego@hotmail.com>
2025-12-04 02:31:54 -0300

    chora: add environment for ngrokj

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import os
+
 if __name__ == "__main__":
-    from pyngrok import ngrok
-
-    ngrok.set_auth_token("36BG38mU2cM0AEYrt5sTThnaMwF_U9JCQ9ryUiXLnsYNoRnT")
-
-    public_url = ngrok.connect(8000, bind_tls=True)
-
-    print(public_url)
-
     import uvicorn
 
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+    ENV = os.getenv("APP_ENV", "local")
+
+    if ENV == "local":
+        from pyngrok import ngrok
+
+        ngrok.set_auth_token("36BG38mU2cM0AEYrt5sTThnaMwF_U9JCQ9ryUiXLnsYNoRnT")
+        tunnel = ngrok.connect(8000, bind_tls=True)
+        print("➡️  Ngrok Tunnel:", tunnel.public_url)
+
+    uvicorn.run(
+        "src.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=(ENV == "local"),
+    )
 
